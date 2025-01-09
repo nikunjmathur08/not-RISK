b682cc41ebcf628e161c94b140c76c3c27e2c519
Nikunj Mathur <nikunjmathur0810@gmail.com>
2025-01-09 23:29:52 +0530

    feat: add basic code for navbar

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,23 @@
+
+
+function HomePage() {
+  return (
+    <>
+      <nav className="my-6 mx-4 flex justify-between">
+        <div className="font-bold text-xl pl-3">
+          <p>!RISK</p>
+        </div>
+        <div className="pr-3">
+          <button className="pr-3">
+            hi, name!
+          </button>
+          <button>
+            image
+          </button>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default HomePage
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: {
+          700: "#7848F4"
+        }
+      }
+    },
   },
   plugins: [],
 }
