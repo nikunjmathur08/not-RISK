e6d03c25981096cee123092f96136151d5061fe6
Nikunj Mathur <nikunjmathur0810@gmail.com>
2025-01-11 19:36:18 +0530

    feat: add footer component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import ApplianceCard from "../components/ApplianceCard";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 function HomePage() {
@@ -6,7 +7,7 @@ function HomePage() {
     <div>
       <div className="mx-4">
         <Navbar />
-        <p className="pl-3 text-xl font-medium">your appliances</p>
+        <p className="pl-3 text-xl font-bold">your appliances</p>
         <div className="mt-10 ml-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-8">
           <ApplianceCard companyName="Haier" applianceName="Haier Refrigerator" applianceImg="/temp/Refrigerator.jpg" />
           <ApplianceCard companyName="Haier" applianceName="Haier Refrigerator" applianceImg="/temp/Refrigerator.jpg" />
@@ -23,15 +24,7 @@ function HomePage() {
           </button>
         </div>
       </div>
-      <div className="bg-blue-900 pt-16 text-center">
-        <button className="bg-slate-50 rounded-lg py-4 px-6 mb-4 inline-block">add appliance</button>
-        <div className="flex justify-center space-x-8 text-white font-light mb-4">
-          <p>home</p>
-          <p>about</p>
-          <p>services</p>
-          <p>FAQs</p>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
